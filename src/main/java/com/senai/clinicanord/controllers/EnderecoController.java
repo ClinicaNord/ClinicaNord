1ecc5ad681b3ad6530e9e8aea0d7e964a30f9848
ju <juliacstro1@gmail.com>
2025-09-18 14:16:10 -0300

    Arrumando o endereco controller
    tinha apagado sem querer o import

--- src/main/java/com/senai/clinicanord/controllers/EnderecoController.java
+++ src/main/java/com/senai/clinicanord/controllers/EnderecoController.java
@@ -12,6 +12,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.senai.clinicanord.entities.Endereco;
+import com.senai.clinicanord.services.EnderecoService;
+
 @RestController
 @RequestMapping("/cadastroendereco")
 public class EnderecoController {
