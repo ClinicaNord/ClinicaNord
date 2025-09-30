ae8ac9f09c408ba7272cb2a53c089c2b0246b939
TuannyThomazelli <tuanny.thomazelli@gmail.com>
2025-09-30 10:53:47 -0300

    Criação do data.sql
    Foi criado o data.sql para: ter a opção de cliente e admin e para os serviços que a clinica presta

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT IGNORE INTO tb_tipo_usuario (id_tipo_usuario, nome) VALUES
+(1, 'Admin'),
+(2, 'Cliente');
+
+INSERT IGNORE INTO tb_tipo (id_servico, nomeServicos) VALUES
+(1, 'FonoTerapia'),
+(2, 'Audiometria Clínica'),
+(3, 'Reabilitação Auditiva');
