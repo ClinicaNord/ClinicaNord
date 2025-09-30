098fcdb4d631806867d1ffceff71962f09a9bba8
jucastrox <159180407+jucastrox@users.noreply.github.com>
2025-09-30 15:02:02 -0300

    alteracoes no data SQL
    ved arrumou pra gnt

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,8 +1,8 @@
-INSERT IGNORE INTO tb_tipoUsuario (idTipoUsuario, nome) VALUES
+INSERT IGNORE INTO tb_tipo_usuario (id_tipo_usuario, nome) VALUES
 (1, 'Admin'),
 (2, 'Cliente');
 
-INSERT IGNORE INTO tb_servicos (idServicos, nome) VALUES
+INSERT IGNORE INTO tb_servicos (id_servicos, nome_servicos) VALUES
 (1, 'Audiometria Clínica'),
 (2, 'FonoTerapia'),
 (3, 'Reabilitação Auditiva');
