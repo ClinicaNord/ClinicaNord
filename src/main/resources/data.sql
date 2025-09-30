6c620c915fa655e65d6610e3cae2193b1c53a81f
TuannyThomazelli <tuanny.thomazelli@gmail.com>
2025-09-30 13:46:08 -0300

    data.sql
    criação do data.sql

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+INSERT IGNORE INTO tb_tipoUsuario (idTipoUsuario, nome) VALUES
+(1, 'Admin'),
+(2, 'Cliente');
+
+INSERT IGNORE INTO tb_servicos (idServicos, nome) VALUES
+(1, 'Audiometria Clínica'),
+(2, 'FonoTerapia'),
+(3, 'Reabilitação Auditiva');
