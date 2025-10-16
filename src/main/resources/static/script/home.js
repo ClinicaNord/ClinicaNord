be8f48cf9243099477f8b32a53b829f84160bb79
Gusleme <gustavolemecastro@gmail.com>
2025-10-16 14:48:18 -0300

    script da pagina listaPaciente
    agora a pagina lista os clientes além de quando for clicado no nome mostrará os seus agendamentos

--- src/main/resources/static/script/home.js
+++ src/main/resources/static/script/home.js
@@ -3,11 +3,10 @@ document.addEventListener("DOMContentLoaded", () => {
   const btnAgendar = document.querySelector(".hero button");
   btnAgendar.addEventListener("click", () => {
     const usuarioLogado = localStorage.getItem("usuarioLogado");
-    if (usuarioLogado) {
-      window.location.href = "./tiposTerapia.html";
-    } else {
-      alert("Você precisa estar cadastrado para agendar uma avaliação!");
+    if (!usuarioLogado) {
       window.location.href = "./cadastroCliente.html";
+    } else {
+      window.location.href = "./tiposTerapia.html";
     }
   });
 });
