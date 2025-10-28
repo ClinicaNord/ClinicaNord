fc0feba7d75be6653c56c5355a659934360fbbaf
Isaagapito <isabeleagapito.dev@gmail.com>
2025-10-28 09:28:20 -0300

    Perfil Cliente
    Nova página do perfil cliente

--- src/main/resources/static/script/perfil.js
+++ src/main/resources/static/script/perfil.js
@@ -18,8 +18,10 @@ document.addEventListener('DOMContentLoaded', () => {
   // Depois, atribui ao conteúdo de texto interno (textContent) desse elemento o valor da propriedade 'nome' do objeto 'usuario'.
   // O operador lógico OR (||) é usado para garantir que, caso 'usuario.nome' seja undefined, null ou vazio, o conteúdo do elemento será definido como uma string vazia (''),
   // evitando que apareça 'undefined' ou erro na página.
-  document.getElementById('nome').textContent = usuario.nomeUsuario || '';
+  document.getElementById('nome').textContent = usuario.nome || '';
   document.getElementById('email').textContent = usuario.email || '';
+  document.getElementById('telefone').textContent = usuario.telefone || '';
+  document.getElementById('dataNascimento').textContent = usuario.dataNascimento || '';
 
   // Configura botão logout para limpar o localStorage e voltar para login
   document.getElementById('logoutBtn').addEventListener('click', () => {
