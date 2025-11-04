1e9ce8428e3472045bb7aee9807a6937c2fb867f
Gusleme <gustavolemecastro@gmail.com>
2025-11-04 13:04:00 -0300

    tentativa de arrumar o cadastro convenio

--- src/main/java/com/senai/clinicanord/controllers/CarteirinhaController.java
+++ src/main/java/com/senai/clinicanord/controllers/CarteirinhaController.java
@@ -3,50 +3,36 @@ package com.senai.clinicanord.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.senai.clinicanord.entities.Carteirinha;
 import com.senai.clinicanord.services.CarteirinhaService;
 
-
+@CrossOrigin(origins = "http://localhost:5500") // ou a porta onde seu front está
 @RestController
 @RequestMapping("/carteirinha")
 public class CarteirinhaController {
 
-	//ATRIBUTOS
     @Autowired
     private CarteirinhaService carteirinhaService;
 
-    //MÉTODOS
-    //método para postar/salvar um novo convenio
     @PostMapping
     public Carteirinha createCarteirinha(@RequestBody Carteirinha carteirinha) {
         return carteirinhaService.saveCarteirinha(carteirinha);
     }
 
-    //método para listar todos os convenios
     @GetMapping
     public List<Carteirinha> getAllCarteirinha() {
         return carteirinhaService.getAllCarteirinha();
     }
 
-    //método para buscar o convenio pelo id
     @GetMapping("/{id}")
-    public Carteirinha getCarteirinha(@PathVariable Long idCarteirinha) {
+    public Carteirinha getCarteirinha(@PathVariable("id") Long idCarteirinha) {
         return carteirinhaService.getCarteirinhaById(idCarteirinha);
     }
 
-
-    //método para deletar o convenio pelo id
     @DeleteMapping("/{id}")
-    public void deleteCarteirinha (@PathVariable Long idCarteirinha) {
+    public void deleteCarteirinha(@PathVariable("id") Long idCarteirinha) {
         carteirinhaService.deleteCarteirinha(idCarteirinha);
-    
-}
+    }
 }
